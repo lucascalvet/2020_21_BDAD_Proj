cdcc2f41d73ac2fe150f4eb16600ce33990354cd
Frederico Rodrigues <58302405+jfred24@users.noreply.github.com>
2021-05-23 22:12:29 +0100

    gatilho3 complete

--- gatilho3_verifica.sql
+++ gatilho3_verifica.sql
@@ -1,4 +1,7 @@
-UPDATE User SET balance=100 WHERE username='lucascs';
-DELETE FROM Purchase WHERE user='lucascs' AND game=4;
-INSERT INTO Purchase VALUES('lucascs', 4, NULL, strftime('%s', '2021-01-05'), 1);
-SELECT * FROM Purchase WHERE user='lucascs' AND game=4;
+UPDATE User SET balance=140 WHERE username='lucascs';
+DELETE FROM Purchase WHERE user='lucascs' AND (game = 4 or game = 5 or game = 6);
+INSERT INTO Purchase VALUES('lucascs', 4, NULL, strftime('%s', '2021-01-05'), 1); -- will get the price automatically and is on sale (50%)
+INSERT INTO Purchase VALUES('lucascs', 5, 90.0, strftime('%s', '2021-01-05'), 1); -- user will pay 90
+INSERT INTO Purchase VALUES('lucascs', 6, NULL, strftime('%s', '2021-01-05'), 1); -- will not have enough balance
+SELECT * FROM Purchase WHERE user='lucascs' AND (game = 4 or game = 5 or game = 6);
+SELECT balance from User where username = 'lucascs';
